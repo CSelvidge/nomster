4a421385a26a4fd41fdeea42b9017b32d02f86e5
Cory Selvidge <healincheese@gmail.com>
2019-05-27 20:30:02 +0000

    Geolocation added

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,10 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+
+  
+  validates :name, length: {minimum: 3}, uniqueness: true
+  validates :address, presence: true
+  validates :description, presence: true
 end
