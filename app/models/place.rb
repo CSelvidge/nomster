3b4dae58c823493ab849549e56a609a1884aece5
Cory Selvidge <healincheese@gmail.com>
2019-06-08 23:51:03 +0000

    Working on emails

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,7 +1,7 @@
 class Place < ApplicationRecord
   belongs_to :user
   has_many :comments
-  
+
   geocoded_by :address
   after_validation :geocode
 
@@ -9,4 +9,6 @@ class Place < ApplicationRecord
   validates :name, length: {minimum: 3}, uniqueness: true
   validates :address, presence: true
   validates :description, presence: true
+
+  has_many :photos
 end
